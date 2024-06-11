fd746c7e60f83989246593016fe2583a6403feed
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-06-10 22:44:11 -0500

    plantilla inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>home</h1>
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
